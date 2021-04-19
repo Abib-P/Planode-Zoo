d101da66b61f5202b6bcfb85b52a8b1e496d7de2
Clement <clementhottotpro@gmail.com>
2021-04-19 20:43:41 +0200

    added space type controller and rooter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import {Express} from "express";
 import {jobRouter} from "./job.route";
 import {authClientRouter} from "./authClient.router";
+import {spaceTypeRouter} from "./spaceType.roote";
 
 export function buildRoutes(app: Express){
     app.use("/job", jobRouter);
     app.use("/client", authClientRouter);
+    app.use("/spaceType", spaceTypeRouter);
 }
