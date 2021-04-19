ef235cfbea86b9748dd2ec79f27e928d7cbd991f
Clement <clementhottotpro@gmail.com>
2021-04-19 20:56:17 +0200

    added media type controller and rooter

---
+++ src/controllers/mediaType.controller.ts
@@ -0,0 +1,66 @@
+import {ModelCtor} from "sequelize";
+import {SequelizeManager} from "../models";
+import {MediaTypeCreationProps, MediaTypeInstance, MediaTypeProps} from "../models/mediaType.model";
+
+
+export class MediaTypeController {
+
+    MediaType: ModelCtor<MediaTypeInstance>;
+
+    private static instance: MediaTypeController;
+
+    public static async getInstance(): Promise<MediaTypeController> {
+        if (MediaTypeController.instance === undefined) {
+            const {MediaType} = await SequelizeManager.getInstance();
+            MediaTypeController.instance = new MediaTypeController(MediaType);
+        }
+        return MediaTypeController.instance;
+    }
+
+    private constructor(MediaType: ModelCtor<MediaTypeInstance>) {
+        this.MediaType = MediaType;
+    }
+
+    public async create(props: MediaTypeCreationProps): Promise<MediaTypeInstance | null> {
+        return this.MediaType.create(props);
+    }
+
+    public async getOne(id: number): Promise<MediaTypeInstance | null> {
+        return this.MediaType.findOne({
+            where: {
+                id
+            }
+        });
+    }
+
+    public async getAll(): Promise<MediaTypeInstance[] | null> {
+        return this.MediaType.findAll();
+    }
+
+    public async update(props: MediaTypeProps): Promise<MediaTypeInstance | null> {
+        const mediaType = await MediaTypeController.instance.getOne(props.id);
+        if (mediaType != null){
+            return mediaType.update(
+                {
+                    name: props.name
+                }
+            );
+        }
+        return null;
+    }
+
+    public async delete(id: number): Promise<number> {
+        const mediaType = await MediaTypeController.instance.getOne(id);
+        if (mediaType != null){
+            return this.MediaType.destroy(
+                {
+                    where: {
+                        id: mediaType.id
+                    }
+                }
+            );
+        }
+        return 0;
+    }
+
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import {Express} from "express";
 import {jobRouter} from "./job.route";
 import {authClientRouter} from "./authClient.router";
 import {spaceTypeRouter} from "./spaceType.roote";
+import {mediaTypeRouter} from "./mediaType.roote";
 
 export function buildRoutes(app: Express){
     app.use("/job", jobRouter);
     app.use("/client", authClientRouter);
     app.use("/spaceType", spaceTypeRouter);
+    app.use("/mediaType", mediaTypeRouter);
 }
